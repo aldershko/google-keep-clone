62da60c0e04277d8eebc072b4dbda2ccc92e2ab6
aldershko <iamalengeorge7@gmail.com>
2023-05-15 11:58:56 +0530

    refactor:make the basic login page template

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const test = () => {
   return (
-    <div className='text-3xl font-bold underline'>test</div>
+    <div className="min-h-screen flex">
+        <div className="flex-1 ...">
+          <div className="flex flex-row min-h-screen shadow-inner bg-slate-500 justify-center items-center">
+            <div className="flex flex-col h-128 w-128 p-10 md:w-128 sm:w-96 m-10  justify-between rounded-lg shadow-xl items-center bg-gray-200 text-black">
+              <h2 className='text-3xl font-bold
+              mb-5'>Login</h2>
+             <input className='m-5 p-5 rounded-lg' type='text' placeholder='username'/>
+             <input className='m-5 p-5 rounded-lg' type='password' placeholder="password"/>
+            </div>
+          </div>
+        </div>
+    </div>
   )
 }
 
